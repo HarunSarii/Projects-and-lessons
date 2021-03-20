94ab33f7363a6d4f615f6f70d19d00554205cf1c
HarunSarii <harunsari14@gmail.com>
2021-03-20 17:20:12 +0300

    add new files

---
+++ app.js
@@ -0,0 +1,76 @@
+// document.write('Hello!<br>');
+// document.write('Clarusway');
+// console.log('Helloo!\nClaruswayy');
+// console.log('Claruswayy');
+// alert('hello\nclarusway!')
+// confirm("Hello")
+// prompt("enter your age:", 17);
+// document.body.innerHTML = '<h1>Clarusway Rocks!</h1>';
+// document.innerHTML = "let'start";
+// var 1a;
+// console.info('an info');
+// console.error('an error occured');
+// console.timeEnd('event');
+
+// var a = "merhaba";
+// console.log(typeof a)
+
+// var name1 = "mark";
+// console.log('Merhaba ${name1}');
+
+// console.log((0.1+0.2).toFixed(2))
+
+// document.getElementById("demo").innerHTML ="Hello Dolly!";
+
+
+// var myCar = new Object();
+// myCar.make = 'Ford';
+// myCar.model = 'Mustang';
+// myCar.year = 1969;
+
+// var life = 100;
+// console.log(life);
+//  var name = "Dev Ed"; // his name
+
+// console.log(name, life);
+
+// life = life -10;
+
+
+// // console.log(life);
+
+// var time = 12;
+// if (time < 8) {
+//     console.log("Good morning.");
+// } else {
+//     console.log("Good day.");
+// }
+
+// var time = 21.00;
+// if (time < 8) {
+//     console.log("Good morning.");
+// } else if (time < 18) {
+//     console.log("Good afternoon.");
+// } else {
+//     console.log("Good evening.");
+// }
+
+// var name = 'Edward';
+// switch (name) {
+//   case 'John':
+//     console.log(name + ' is a instructor.');
+//     break;
+//   case 'Walter':
+//   case 'James':
+//     console.log(name + ' is a mentor.');
+//     break;
+//   default:
+//     console.log('Sorry, i dont know ' + name+ '.');
+// // }
+
+// 5+3;
+
+// "CLARUSWAY" > "Clarusway";
+
+
+let operator = ("işlem giriniz")
