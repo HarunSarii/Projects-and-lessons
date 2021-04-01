19f5e908ac5f6a12f235302f233dd89a52b39599
HarunSarii <harunsari14@gmail.com>
2021-04-01 19:34:00 +0300

    add dom

---
+++ DOM/app-dom.js
@@ -0,0 +1,36 @@
+// // const text = document.querySelector(".title");
+// // const changeColor = document.querySelector(".changeColor");
+// const userList = document.querySelector(".name-list");
+// const listInput = document.querySelector(".list-input");
+// const addListButton = document.querySelector(".addListButton");
+// // console.log(listInput.value);
+
+// addListButton.addEventListener("click", function(){
+//     const newLi = document.createElement("LI");
+//     const liContent = document.createTextNode("listInput.value");
+//     // console.log(listInput.value);
+//     newLi.appendChild(liContent);
+//     userList.appendChild(newLi);
+// });
+
+// text.style.backgroundColor = "red";
+// text.classList.add("change");
+// text.classList.remove("change");
+
+// changeColor.addEventListener("click",function(){
+//     text.classList.add("change");
+// });
+
+// changeColor.addEventListener("click",function(){
+//     text.classList.toggle("change");
+// });
+
+// const userList = document.querySelectorAll(".name-list li");
+// // console.log(userList);
+
+// for ( user of userList){
+//     user.addEventListener("click", function() {
+//        console.log(this); 
+//     // this.style.color = "red";
+//     });
+// }
