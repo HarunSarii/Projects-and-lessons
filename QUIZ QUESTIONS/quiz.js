4a77770f5c3fde97631cceb185d925e87327a2ff
HarunSarii <harunsari14@gmail.com>
2021-04-12 16:23:01 +0300

    add quiz questions

--- QUIZ QUESTIONS/quiz.js
+++ QUIZ QUESTIONS/quiz.js
@@ -13,3 +13,44 @@
 // console.log(s);
 // var arr = [7, 3, 5, 9];
 // arr.reverse().find(n=>n%3); //output:5
+
+// for (var i = 1; i < 3; i++)
+// {
+//     console.log(i);
+//     setTimeout(function(){
+//         console.log(i);}, i*1000);
+//     }
+// console.log(i+2); 
+
+// output: 1 2 5 3 3 
+
+// var price = [
+//     {pr$: 700, name: 'pc'},
+//     {pr$: 30, name: 'book'},
+//     {pr$: 90, name: 'pen'}
+
+// ];
+// const lower100 = price.map(n => n.pr$ < 100 && console.log(n));
+
+// output: {pr$: 30, name: "book"} 
+//         {pr$: 90, name: "pen"}
+
+// var x = ["a", "b", "c"];
+// var y = ["a", "b", "c"];
+// console.log(x == y);
+// console.log(x === x.reverse());
+// console.log(x);
+
+// output:
+// false
+// true
+// (3) ["c", "b", "a"]
+
+// const $ = [10, 100];
+// const euro = $.forEach(n => console.log(n*0.82));
+// console.log(euro);
+
+//output:
+// 8.2
+// 82
+// undefined
