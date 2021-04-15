2a36184f5a2af25fa173dd8d0e1b9daf72758979
HarunSarii <harunsari14@gmail.com>
2021-04-15 20:55:13 +0300

    add udemy

--- Dice-Game/app.js
+++ Dice-Game/app.js
@@ -7,33 +7,57 @@ const diceElm = document.querySelector('.dice');
 diceElm.classList.add('hidden');
 
 const btnRoll = document.querySelector('.btn--roll');
-
 btnRoll.addEventListener('click', clickRollDice);
-
-let activePlayer = 0;
-
-let currentScore = 0;
-const currentScoreElm0 = document.getElementById('current--0');
-
-// const currentScoreElms = [];
-// for (let i = 0; i < 2; i++) {
-//   currentScoreElms.push(document.getElementById(`current--${i}`));
-// }
-const currentScoreElms = document.querySelectorAll('.current-score');
-
-const sectionPlayers = document.querySelectorAll('.player');
 
 function clickRollDice() {
   diceElm.classList.remove('hidden');
   const diceNumber = Math.trunc(Math.random() * 6 + 1);
-  // console.log(diceNumber);
   diceElm.src = `dice-${diceNumber}.png`;
-  if (diceNumber === 1) {
-    currentScore = 0;
-    currentScoreElms[avtivePlayer].textContent = 0;
-    activePlayer = 1 - activePlayer;
-  }
-  currentScore += diceNumber;
-  // currentScoreElm0.textContent = currentScore;
-  currentScoreElms[activePlayer].textContent = currentScore;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btnRoll = document.querySelector('.btn--roll');
+
+// btnRoll.addEventListener('click', clickRollDice);
+
+// let activePlayer = 0;
+
+// let currentScore = 0;
+// const currentScoreElm0 = document.getElementById('current--0');
+
+// // const currentScoreElms = [];
+// // for (let i = 0; i < 2; i++) {
+// //   currentScoreElms.push(document.getElementById(`current--${i}`));
+// // }
+// const currentScoreElms = document.querySelectorAll('.current-score');
+
+// const sectionPlayers = document.querySelectorAll('.player');
+
+// function clickRollDice() {
+//   diceElm.classList.remove('hidden');
+//   const diceNumber = Math.trunc(Math.random() * 6 + 1);
+//   // console.log(diceNumber);
+//   diceElm.src = `dice-${diceNumber}.png`;
+//   if (diceNumber === 1) {
+//     currentScore = 0;
+//     currentScoreElms[avtivePlayer].textContent = 0;
+//     activePlayer = 1 - activePlayer;
+//   }
+//   currentScore += diceNumber;
+//   // currentScoreElm0.textContent = currentScore;
+//   currentScoreElms[activePlayer].textContent = currentScore;
+// }
