74435ae391cf5a5047b2e31280031db7b8f9a9dd
HarunSarii <harunsari14@gmail.com>
2021-05-02 00:16:44 +0300

    update promise

--- java-async/promise.js
+++ java-async/promise.js
@@ -1,6 +1,6 @@
 const startTime = Date.now();
 const myLog = (input) => {
-  console.log(`Elapsed: ${Date.now()} - startTime}ms/t/t${input}`);
+  console.log(`Elapsed: ${Date.now() - startTime}ms \t\t${input}`);
 };
 
 const slowTask = () => {
@@ -11,4 +11,48 @@ const slowTask = () => {
   return "task is completed.";
 };
 
-myLog("1. Synchronous");
+// myLog("1. Synchronous");
+
+// setTimeout(() => {
+//   slowTask();
+//   myLog("2. Timeout");
+// }, 0);
+
+// Promise.resolve().then(() => {
+//   slowTask();
+//   myLog("3. Promise");
+// });
+
+// myLog("4. Synchronous");
+
+// const myPromise = new Promise((resolve, reject) => {
+//   console.log("promise started");
+//   /* ---- */
+//   resolve("task completed");
+// });
+
+// myPromise
+//   .then((msg) => {
+//     console.log(msg);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const myPromise = new Promise((resolve, reject) => {
+  myLog("promise started");
+  /* ---- */
+  resolve("task completed");
+});
+
+myPromise
+  .then((msg) => {
+    myLog(msg);
+  })
+  .catch((err) => {
+    myLog(err);
+  });
+
+const brewTea = newPromise()
+  .resolve()
+  .then(() => {});
