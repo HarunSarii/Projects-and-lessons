774cddc8cece87f862a40470d0bebfc669bbe1a4
HarunSarii <harunsari14@gmail.com>
2021-05-03 01:00:33 +0300

    update react-tour

--- React-tour-proj/src/App.js
+++ React-tour-proj/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
+import Card from "./components/cards/Card";
+import "/App.css";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Card />
     </div>
   );
 };
