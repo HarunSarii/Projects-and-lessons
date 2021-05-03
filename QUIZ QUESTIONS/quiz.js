f47bcaaed539ceedddf058e815f3fdc4fc381f46
HarunSarii <harunsari14@gmail.com>
2021-05-03 13:46:56 +0300

    update quiz

--- QUIZ QUESTIONS/quiz.js
+++ QUIZ QUESTIONS/quiz.js
@@ -311,3 +311,11 @@
 //   })(2);
 // })(1);
 // output:1
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1);
+}
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1);
+}
+// // 3 3 3 and 0 1 2
